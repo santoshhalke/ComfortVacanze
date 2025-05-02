f47b967245dc8989b14b0e801fc23876192cb82f
harshada <11harshadapatil@gmail.com>
2025-05-02 15:45:41 +0530

    whatsapp icon added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-
+import { FaWhatsapp } from 'react-icons/fa';
 import Home2 from "@/components/Home/Home2";
 import HeroCarousel from "@/components/Home/Home1"
 import DestinationsSection from "@/components/Home/Home3"
 import ServicesSection from "@/components/Home/Home4";
 import TestimonialsPage from "@/components/Home/Home5";
+import Link from 'next/link';
 export default function Home() {
   return (
     <div>
@@ -12,7 +13,14 @@ export default function Home() {
 <DestinationsSection/>
 <ServicesSection/>
 <TestimonialsPage/>   
-
+<Link
+        href="https://wa.me/919999999999" // Replace with your actual WhatsApp number
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 text-white rounded-full p-3 shadow-lg transition duration-300 ease-in-out"
+      >
+        <FaWhatsapp className="w-6 h-6" />
+      </Link>
 
       {/*Last Section */}
       
